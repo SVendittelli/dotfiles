febccb783fccb61f0c10fcc295daf8240a3557fa
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-01-07 18:57:14 +0000

    Add directory zsh aliases

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -1,11 +1,49 @@
 # common aliases
-alias ls='ls --color=auto'
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
 
+## Dirsctories aliases
+# Changing/making/removing directory
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias md='mkdir -p'
+alias rd=rmdir
+
+function d() {
+    if [[ -n $1 ]]; then
+        dirs "$@"
+    else
+        dirs -v | head -n 10
+    fi
+}
+compdef _dirs d
+
+## ls aliases
+alias ls='ls --color=auto'
+alias l='ls -lFh'   #size,show type,human readable
+alias la='ls -lAFh' #long list,show almost all,show type,human readable
+alias lr='ls -tRFh' #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh' #long list,sorted by date,show type,human readable
+alias ll='ls -l'    #long list
+
+## Improve grep
 alias grep='grep --color'
 alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
 
@@ -17,20 +55,23 @@ alias -g I='| grep -i'
 alias -g L="| less"
 alias -g M="| most"
 
+## Helpful system aliases
 alias help='man'
 alias p='ps -f'
 alias unexport='unset'
-
-alias g='git'
-
 alias update="sudo apt update && sudo apt upgrade -y"
 
+## Shorten commands
+alias g='git'
 alias x="exit"
 
-alias edf="chezmoi edit --apply"
-
+## Reload zsh config
 alias reload="source ~/.zshenv && source ~/.zshrc"
-alias zshconfig="chezmoi edit --apply ~/.zshrc"
+
+## Edit dotfiles
+alias edf="chezmoi edit --apply"
+alias i3config="edf ~/.config/i3/config"
+alias zshconfig="edf ~/.zshrc"
 
 # List processes on port
 function on-port {
