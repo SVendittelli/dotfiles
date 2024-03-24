f2dfcfebcd3f3fa2340b18a628160a4273b72acf
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-03-24 23:33:41 +0000

    feat: reuse kitty conf

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -1,66 +1,17 @@
 # common aliases
 
-## Directories aliases
-# Changing/making/removing directory
-setopt auto_cd
-setopt auto_pushd
-setopt pushd_ignore_dups
-setopt pushdminus
-
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
-
-alias -- -='cd -'
-alias 1='cd -1'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
-
-alias md='mkdir -p'
-alias rd=rmdir
-
-function d() {
-    if [[ -n $1 ]]; then
-        dirs "$@"
-    else
-        dirs -v | head -n 10
-    fi
-}
-compdef _dirs d
-
 ## ls aliases
 alias ls='eza'
 alias l='ls -lh'         #size,show type,headers
 alias la='ls -lah --git' #long list,show almost all,show type,headers
 alias ll='ls -l'         #long list
 
-## Improve grep
-alias grep='grep --color'
-alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
-
-## Command line head / tail shortcuts
-alias -g H='| head'
-alias -g T='| tail'
-alias -g G='| grep'
-alias -g I='| grep -i'
-alias -g L="| less"
-alias -g M="| most"
-
 ## Helpful system aliases
 alias help='tldr'
-alias p='ps -f'
-alias unexport='unset'
 
 ## Shorten commands
 alias e=$EDITOR
-alias e.='$EDITOR .'
+alias e.='e .'
 alias g='git'
 alias x="exit"
 
@@ -75,6 +26,7 @@ alias reload="source ~/.zshenv && source ~/.zshrc"
 ## Edit dotfiles
 alias edf="chezmoi edit --apply"
 alias zshconfig="edf ~/.zshrc"
+alias udf="chezmoi --refresh-externals update && reload"
 
 ## Edit NixOS config
 alias enos="$EDITOR ~/nixos"
@@ -84,3 +36,4 @@ alias anos="~/nixos/nixos-rebuild.sh"
 function on-port {
     lsof -i :"$@"
 }
+
