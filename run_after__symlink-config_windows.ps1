595561592a7eb42ca6983ed53254b3cef07a2440
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-04-15 10:02:53 +0100

    feat: add alacritty config

--- run_after__symlink-config_windows.ps1
+++ run_after__symlink-config_windows.ps1
@@ -8,5 +8,10 @@ If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
   If (-Not (Test-Path $PROFILE.CurrentUserAllHosts)) {
     New-Item -ItemType SymbolicLink -Path $PROFILE.CurrentUserAllHosts -Target $env:USERPROFILE\.config/powershell/profile.ps1
   }
+  #
+  # symlink powershell profile
+  If (-Not (Test-Path $env:APPDATA\alacritty\alacritty.toml)) {
+    New-Item -ItemType SymbolicLink -Path $env:APPDATA\alacritty\alacritty.toml -Target $env:USERPROFILE\.config/alacritty/alacritty.toml
+  }
 }
 
