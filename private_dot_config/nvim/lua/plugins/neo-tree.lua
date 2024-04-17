acb2b570bff1bbe294a509d5a42736cd744a8d01
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-04-17 13:37:02 +0100

    feat: update vim preferences

--- private_dot_config/nvim/lua/plugins/neo-tree.lua
+++ private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,14 +1,17 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	opts = {
-		filesystem = {
-			filtered_items = {
-				hide_dotfiles = false,
-				never_show = {
-					"node_modules",
-					".git",
-				},
-			},
-		},
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        never_show = {
+          "node_modules",
+          ".git",
+        },
+      },
+    },
+    window = {
+      position = "right",
+    },
+  },
 }
