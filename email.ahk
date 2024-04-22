9d3e583c00485f694b9970a4f6210e5ed3bf202b
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-04-22 18:43:15 +0100

    feat: manage autohotkey scripts that autostart

---
+++ email.ahk
@@ -0,0 +1,2 @@
+#Requires AutoHotkey >=2.0  ; https://www.autohotkey.com/docs/v2/lib/_Requires.htm
+::@@::sam.vendittelli@hotmail.com
--- run_after_00_symlink-config_windows.ps1
+++ run_after_00_symlink-config_windows.ps1
@@ -8,10 +8,20 @@ If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
   If (-Not (Test-Path $PROFILE.CurrentUserAllHosts)) {
     New-Item -ItemType SymbolicLink -Path $PROFILE.CurrentUserAllHosts -Target $env:USERPROFILE\.config\powershell\profile.ps1
   }
-  #
+
   # symlink powershell profile
   If (-Not (Test-Path $env:APPDATA\alacritty\alacritty.toml)) {
     New-Item -ItemType SymbolicLink -Path $env:APPDATA\alacritty\alacritty.toml -Target $env:USERPROFILE\.config\alacritty\alacritty.toml
   }
+
+  # symlink email autohotkey script
+  $StartupFolder = [Environment]::GetFolderPath('Startup')
+  If (-Not (Test-Path $StartupFolder\email.ahk.lnk)) {
+    echo $StartupFolder\email.ahk.lnk
+    New-Item -ItemType SymbolicLink -Path $StartupFolder\email.ahk.lnk -Target $env:USERPROFILE\email.ahk
+  }
+
+  #enable komorebi to start automatically
+  komorebic.exe enable-autostart --whkd
 }
 
