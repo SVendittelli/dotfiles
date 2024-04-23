04184b9ce9679c4c45543adfc03b35339cc82058
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-04-23 14:52:50 +0100

    feat: install fonts on windows

---
+++ run_after_01_install-fonts_windows.ps1
@@ -0,0 +1,11 @@
+# Based upon: https://blog.simontimms.com/2021/06/11/installing-fonts/
+$Fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+
+foreach($File in Get-ChildItem -Path $Env:USERPROFILE\fonts -File *.ttf)
+{
+  $FileName = $File.Name
+  if (-not(Test-Path -Path "C:\Windows\Fonts\$FileName" )) {
+        $Fonts.CopyHere($File.fullname) # Installs the font
+        Copy-Item -Path $File.fullname -Destination "C:\Windows\Fonts\$FileName" # Moves the font to the folder so we don't install multiple times
+    }
+}
