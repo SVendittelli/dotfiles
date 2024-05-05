93df05ec39d0a26bcc9d826317c72e6395d5c8bf
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-05-05 16:45:29 +0100

    fix(nvim): apply hidden search to grep_string also

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -143,20 +143,21 @@ require('lazy').setup({
         --  All the info you're looking for is in `:help telescope.setup()`
         defaults = {
           file_ignore_patterns = { '.git' },
-          vimgrep_arguments = {
-            'rg',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case',
-            '--hidden',
-          },
         },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+          grep_string = {
+            additional_args = { '--hidden' },
+          },
+          live_grep = {
+            additional_args = { '--hidden' },
           },
         },
       })
@@ -169,9 +170,7 @@ require('lazy').setup({
       -- See `:help telescope.builtin`
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-      vim.keymap.set('n', '<leader>ff', function()
-        builtin.find_files({ hidden = true })
-      end, { desc = '[F]ind [F]iles' })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
       vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
