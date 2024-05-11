1197615f0c4102326890a43d8946a0f05de1d19d
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-05-11 11:52:32 +0100

    feat(pwsh): symlink config

--- .chezmoiscripts/run_after_00_symlink-config.ps1
+++ .chezmoiscripts/run_after_00_symlink-config.ps1
@@ -5,8 +5,11 @@ If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
   }
 
   # symlink powershell profile
-  If (-Not (Test-Path $Profile)) {
+  $ConfigFolder = Split-Path $PROFILE.CurrentUserCurrentHost
+  $ConfigFile = "$ConfigFolder\powershell.config.json"
+  If (-Not ((Test-Path $Profile) -And (Test-Path $ConfigFile))) {
     New-Item -ItemType SymbolicLink -Path $Profile -Target $env:USERPROFILE\.config\powershell\profile.ps1
+    New-Item -ItemType SymbolicLink -Path $ConfigFile -Target $env:USERPROFILE\.config\powershell\powershell.config.json
   }
 
   # symlink alacritty config
