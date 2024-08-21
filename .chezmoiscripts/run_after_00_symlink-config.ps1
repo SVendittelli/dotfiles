a353f878d767766dcd3a50b5d4705881f9bab91a
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-08-21 17:14:01 +0100

    feat(obsidian): start obsidian on startup

--- .chezmoiscripts/run_after_00_symlink-config.ps1
+++ .chezmoiscripts/run_after_00_symlink-config.ps1
@@ -1,26 +1,44 @@
 If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
+  # Create some reusable variables
+  $UserConfigFolder = "$env:USERPROFILE\.config"
+  $StartupFolder = [Environment]::GetFolderPath('Startup')
+
   # symlink the nvim folder into the correct location for windows
-  If (-Not (Test-Path $env:LOCALAPPDATA\nvim)) {
-    New-Item -ItemType Junction -Path $env:LOCALAPPDATA\nvim -Value $env:USERPROFILE\.config\nvim
+  $LinkPath = "$env:LOCALAPPDATA\nvim"
+  $TargetPath = "$UserConfigFolder\nvim"
+  If (-Not (Test-Path $LinkPath)) {
+    New-Item -ItemType Junction -Path $LinkPath -Value $TargetPath
   }
 
-  # copy powershell profile
-  ## as OneDrive syncs this file we must over copy it, not symlink or junction
-  $ConfigFolder = "$Env:USERPROFILE\.config\powershell"
+  # copy powershell profile as OneDrive syncs this file we must over copy it,
+  # not create a symlink or junction
+  $ConfigFolder = "$UserConfigFolder\powershell"
   $ProfileFolder = Split-Path -Parent $Profile
   $DestinationFile = "$ProfileFolder\powershell.config.json"
-  Copy-Item -Path "$ConfigFolder\powershell.config.json" -Destination $ProfileConfigFile
-  Copy-Item -Path "$ConfigFolder\profile.ps1" -Destination $Profile.CurrentUserAllHosts
+  Copy-Item -Path "$ConfigFolder\powershell.config.json" -Destination $DestinationFile
+  Copy-Item -Path "$ConfigFolder\profile.ps1" -Destination $ProfileFolder
 
   # symlink alacritty config
-  If (-Not (Test-Path $env:APPDATA\alacritty)) {
-    New-Item -ItemType Junction -Path $env:APPDATA\alacritty -Value $env:USERPROFILE\.config\alacritty
+  $LinkPath = "$env:APPDATA\alacritty"
+  $TargetPath = "$UserConfigFolder\alacritty"
+  If (-Not (Test-Path $LinkPath)) {
+    New-Item -ItemType Junction -Path $LinkPath -Value $TargetPath
   }
 
-  # symlink email autohotkey script
-  $StartupFolder = [Environment]::GetFolderPath('Startup')
-  If (-Not (Test-Path $StartupFolder\email.ahk.lnk)) {
-    New-Item -ItemType SymbolicLink -Path $StartupFolder\email.ahk.lnk -Target $env:USERPROFILE\email.ahk
+  # symlink email autohotkey script to the startup folder
+  $LinkPath = "$StartupFolder\email.ahk"
+  $TargetPath = "$env:USERPROFILE\email.ahk"
+  If (-Not (Test-Path $LinkPath)) {
+    New-Item -ItemType SymbolicLink -Path $LinkPath -Target $TargetPath
+  }
+
+  # create shortcut for Obsidian in the startup folder
+  $LinkPath = "$StartupFolder\obsidian.lnk"
+  $TargetPath = "$env:LOCALAPPDATA\Programs\Obsidian\obsidian.exe"
+  If (-Not (Test-Path $LinkPath)) {
+    $Link = (New-Object -ComObject WScript.Shell).CreateShortcut($LinkPath)
+    $Link.TargetPath = $TargetPath
+    $Link.Save()
   }
 
   #enable komorebi to start automatically
