1c03c5db6140d7025102aa2df09803ecff142fbe
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-09-13 18:14:52 +0100

    feat(starship): show if chezmoi if not synced

--- dot_local/bin/executable_starship-custom-chezmoi-command.ps1
+++ dot_local/bin/executable_starship-custom-chezmoi-command.ps1
@@ -1,1 +1,19 @@
-Write-Host "[!]"
+&chezmoi git -- diff --quiet HEAD *> $NUL
+if ($LASTEXITCODE -ne 0) {
+    $Changes = '!'
+} else {
+    $Changes = ''
+}
+
+if ($(chezmoi git -- rev-list --right-only --count origin..HEAD) -gt 0) {
+    $Ahead = '+'
+} else {
+    $Ahead = ''
+}
+if ($(chezmoi git -- rev-list --left-only --count origin..HEAD) -gt 0) {
+    $Behind = '-'
+} else {
+    $Behind = ''
+}
+
+return "[$($Changes)$($Ahead)$($Behind)]"
--- dot_local/bin/executable_starship-custom-chezmoi-when.ps1
+++ dot_local/bin/executable_starship-custom-chezmoi-when.ps1
@@ -1,5 +1,8 @@
+$LocalCommit = git rev-parse HEAD
+$RemoteCommit = git rev-parse origin
+
 &chezmoi git -- diff --quiet HEAD *> $NUL
-if ($LASTEXITCODE -ne 0) {
+if ($LASTEXITCODE -ne 0 -or ($LocalCommit -ne $RemoteCommit)) {
     exit 0
 } else {
     exit 1
