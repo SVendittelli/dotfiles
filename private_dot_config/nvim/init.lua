6e9c135fcc20b4565d2483bf10f5a34118a29293
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-09-25 11:57:16 +0100

    feat(nvim): add keymap for signature help

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -271,10 +271,13 @@ require('lazy').setup({
           --  Useful when your language has ways of declaring types without an actual implementation.
           map('gI', require('telescope.builtin').lsp_implementations, '[g]oto [I]mplementation')
 
+          -- Displays signature information about the symbol under the cursor in a floating window.
+          map('gK', vim.lsp.buf.signature_help, 'open signature help')
+
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'type [D]efinition')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
