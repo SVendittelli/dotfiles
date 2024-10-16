10911201dcc5ce8edbddcb59404e3a8aced575aa
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-10-16 18:18:01 +0100

    fix(env): set XDG env vars for cmd as well as pwsh

--- .chezmoiscripts/windows/run_onchange_before_00_set-env-vars.ps1
+++ .chezmoiscripts/windows/run_onchange_before_00_set-env-vars.ps1
@@ -3,6 +3,11 @@ Write-Host ""
 Write-Host "Setting user environment variables..."
 Write-Host ""
 
+## Set XDG Paths
+[System.Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME',"$Env:USERPROFILE\.config", 'User')
+[System.Environment]::SetEnvironmentVariable('XDG_DATA_HOME',"$Env:USERPROFILE\.local\share", 'User')
+[System.Environment]::SetEnvironmentVariable('XDG_STATE_HOME',"$Env:USERPROFILE\.local\state", 'User')
+
 ## Disable powershell telemetry, this needs to be set before the shell loads
 [System.Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT','1', 'User')
 
